cc37590a9b70694a272344f7908b4ca91100057a
jngerhardt <74320008+jngerhardt@users.noreply.github.com>
2020-12-05 17:22:23 -0600

    12/5/2020

---
+++ Comments/commentDrills.py
@@ -0,0 +1,44 @@
+'''
+For this assignment you should read the task, then below the task do what 
+it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a single line comment
+
+#This is a single line comment
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1) Make a single line comment.
+
+#This is a single line comment.
+
+#2) Make a multi-line comment.
+
+'''
+This is a 
+multi line
+comment.
+'''
+
+#3) Make a variable. Then put a comment after it, on the same line.
+
+var = 4 #var is set to 4
+
+#4) Make a single line comment.
+
+#var = 4 has a single line comment after it.
+
+#5) Make a multi-line comment.
+
+'''
+I am making
+a multi
+line comment.
+'''
