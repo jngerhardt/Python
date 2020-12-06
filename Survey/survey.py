ae2cd021b849b17304d563c658768df5cabc91df
jngerhardt <74320008+jngerhardt@users.noreply.github.com>
2020-12-05 18:48:30 -0600

    12/5/2020

---
+++ Survey/survey.py
@@ -0,0 +1,19 @@
+'''
+Created on Dec 5, 2020
+I am making a survey for a project.
+@author: Jayden Gerhardt
+'''
+name = input("What is your name?")
+print(name)
+
+color = input("What is your favorite color?")
+print(color)
+
+city = input("What city did you grow up in?")
+print(city)
+
+friend = input("What is your best friends name?")
+print(friend)
+
+hair = input("What color hair do you have?")
+print(hair)
